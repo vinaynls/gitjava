86fe9512231ffe9933e88a2e8c7b409a381aa06f
Vinay <vinay@nlsinc.com>
2015-05-27 19:40:10 +0530

    java project added

--- gitJava/src/com/nlsinc/git/util/Employee.java
+++ gitJava/src/com/nlsinc/git/util/Employee.java
@@ -1,13 +1,23 @@
-package com.nlsinc.demo.util;
+package com.nlsinc.git.util;
 
 public class Employee {
 	
 	public int empId;
 	public String ename;
+	
 	public Employee(int empId, String ename) {
 		
 		this.empId = empId;
 		this.ename = ename;
 	}
+	
+	public void printDetails()
+	{
+		System.out.println(this.empId + " " + this.ename);
+	}
+	
+	
+	
+	
 
 }
---
+++ gitJava/src/com/nlsinc/git/util/GitDemo.java
@@ -0,0 +1,20 @@
+package com.nlsinc.git.util;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee e = new Employee(1, "netlogic");
+		printInfo();
+		e.printDetails();
+		
+
+	}
+	
+	public static void printInfo()
+	{
+		System.out.println("This is from method print info");
+	}
+
+}
