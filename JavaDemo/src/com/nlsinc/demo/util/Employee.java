05aba8d821e386530555be6f9834a2386fe29031
Vinay <vinay@nlsinc.com>
2015-05-27 16:38:38 +0530

    Initial commit for the first time

---
+++ JavaDemo/src/com/nlsinc/demo/util/Employee.java
@@ -0,0 +1,13 @@
+package com.nlsinc.demo.util;
+
+public class Employee {
+	
+	public int empId;
+	public String ename;
+	public Employee(int empId, String ename) {
+		
+		this.empId = empId;
+		this.ename = ename;
+	}
+
+}
---
+++ JavaDemo/src/com/nlsinc/demo/util/TestDemo.java
@@ -0,0 +1,14 @@
+package com.nlsinc.demo.util;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee e = new Employee(1,"Vinay");
+		
+		System.out.println("Employee name is :" +e.ename);
+
+	}
+
+}
